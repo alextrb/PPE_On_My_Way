e382baf3a726e230ccef80d6caef3689a327eabf
Mathilde <mathilde.bridron@edu.ece.fr>
2017-12-20 10:49:50 +0100

    Ajout de points à Paris

--- app/src/main/java/com/onmyway/ppe/ppe_onmyway/MapsActivity.java
+++ app/src/main/java/com/onmyway/ppe/ppe_onmyway/MapsActivity.java
@@ -39,8 +39,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng paris = new LatLng(48.872156, 2.347464);
+        mMap.addMarker(new MarkerOptions().position(new LatLng(48.872156, 2.347464)).title("Paris"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(paris));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(48.9, 2.347464)).title("Paris2"));
     }
 }
